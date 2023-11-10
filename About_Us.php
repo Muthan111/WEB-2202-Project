b34a688d7e338fb1c9682910fa51bec9d75ec9b8
Muthan111 <muadthaha@gmail.com>
2023-11-11 00:07:26 +0800

    Changed file extensions

---
+++ About_Us.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
+ <title> About Us
+ </title>
+ <meta charset="utf-8"> // meta always inside the <head> element
+ <a href="login.php">Login</a>
+ <a href="Homepage.php">Homepage</a>
+ <a href="About_Us.php">About Us</a>
+ <a href="Reward.php">Rewards</a>
+ <a href="Redeem.php">Redeem</a>
+ <a href="Map.php">Rewards</a>
+ <a href="Profile.php">Profile</a>
+
+ </head>
+ <body>
+    <P>Code goes here</P>  
+ </body>  
+</html>
---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
+ <title> Login
+ </title>
+ <meta charset="utf-8"> 
+ <a href="Homepage.php">Homepage</a>
+ <a href="About_Us.php">About Us</a>
+ <a href="Location.php">Location</a>
+ <a href="Reward.php">Rewards</a>
+ <a href="Redeem.php">Redeem</a>
+ <a href="Map.php">Rewards</a>
+ <a href="Profile.php">Profile</a>
+
+ </head>
+ <body>
+    <P>Code goes here</P>  
+ </body>   
+</html>
