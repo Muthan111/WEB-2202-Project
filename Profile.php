6b53b3163ce9b24c54fcfc267235fdea444bb09e
Muthan111 <muadthaha@gmail.com>
2023-12-10 18:27:09 +0800

    Doing profile page

--- Profile.php
+++ Profile.php
@@ -3,7 +3,7 @@
  <head>
  <title> Profile
  </title>
- <meta charset="utf-8"> // meta always inside the <head> element
+ <meta charset="utf-8"> 
  <a href="login.php">Login</a>
  <a href="Homepage.php">Homepage</a>
  <a href="About_Us.php">About Us</a>
@@ -15,16 +15,30 @@
 
  </head>
  <body>
-    <div>
-      <div>
+    <div class= "divider">
+      <div class = "left_side">
+         <h4></h4>
+         <img src = "../WEB-2202-Project/images/profile_icon.png">
+         <H4>Name</H4>
+         <p></p>
+      </div>
+    
+      <div class ="right_side">
+         <div class = "information" >
+            <h4>Information</h4>
+            <div class = "info_data">
+               <div class = "data">
+                  <h4>Role</h4>
+                  <p>Customer </p>
+               </div>   
 
-      </div>
-    </div>
-    <div>
-      <div>
-         <h4>Role</h4>
-         <p>Customer </p>
-      </div>
-    </div>  
+               <div>
+               <h4>Email</h4>
+                  <p>find a way to get email from database</p>
+               </div>   
+            </div>
+         </div>   
+      </div>  
+   </div>   
  </body>   
 </html>
