9fb6e2d8e6854471702c9737a8dc44704ae92ede
Muthan111 <muadthaha@gmail.com>
2023-12-15 19:46:15 +0800

    Added create table file

--- create-table.php
+++ create-table.php
@@ -5,21 +5,44 @@
         $dbuser = 'root';
         $dbpass = '';
         $dbname = 'User';
-        $conn = mysqli_connect($dbhost,$dbuser,$dbpass, $dbname);
-        if(! $conn){
-            echo 'Connection failure<br>'.mysqli_connect_error();
+        $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+        
+        if (!$conn) {
+            echo 'Connection failure<br>' . mysqli_connect_error();
         }
-        echo 'connection successfully<br>';
-        $sql = "CREATE TABLE Data(
-            id INT AUTO_INCREMENT, name VARCHAR(20) NOT NULL,Email VARCHAR(30), user_Name VARCHAR(30),password VARCHAR(30),  primary key (id))";
+        
+        echo 'Connection successful<br>';
+        
+        // Create the Data table
+        $sqlData = "CREATE TABLE Data (
+            UserID INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(20) NOT NULL,
+            Email VARCHAR(30),
+            user_Name VARCHAR(30),
+            password VARCHAR(30)
+        )";
 
-        if(mysqli_query($conn,$sql)){
-            echo 'Table created successfully';
+        if (mysqli_query($conn, $sqlData)) {
+            echo 'Data table created successfully<br>';
+        } else {
+            echo 'Data table is not created<br>';
         }
-        else{
-            echo 'Table is not created';
+
+        // Create the AdminUsers table
+        $sqlAdminUsers = "CREATE TABLE AdminUsers (
+            AdminID INT AUTO_INCREMENT,
+            UserID INT,
+            PRIMARY KEY (AdminID),
+            FOREIGN KEY (UserID) REFERENCES Data(UserID)
+        )";
+
+        if (mysqli_query($conn, $sqlAdminUsers)) {
+            echo 'AdminUsers table created successfully<br>';
+        } else {
+            echo 'AdminUsers table is not created<br>';
         }
+
         mysqli_close($conn);
         ?>
-    </body>    
-</html>    
+    </body>
+</html>
